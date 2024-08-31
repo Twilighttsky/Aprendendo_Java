6e936446ba418e64139c84d1a9e8f6336d353076
Jikkan <69170077+Twilighttsky@users.noreply.github.com>
2024-08-31 08:47:05 -0300

    Create exercicio1004-BC.java
    Exercício 1004 do Beecrowd ("Produto Simples"), em Java.
    Enunciado:
    
    Leia dois valores inteiros. A seguir, calcule o produto entre estes dois valores e atribua esta operação à variável PROD. A seguir mostre a variável PROD com mensagem correspondente.   
    
    Entrada
    O arquivo de entrada contém 2 valores inteiros.
    
    Saída
    Imprima a mensagem "PROD" e a variável PROD conforme exemplo abaixo, com um espaço em branco antes e depois da igualdade. Não esqueça de imprimir o fim de linha após o produto, caso contrário seu programa apresentará a mensagem: “Presentation Error”.

---
+++ exercicio1004-BC.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ Scanner ler= new Scanner(System.in);
+ int a,b, prod;
+ a=ler.nextInt();
+ b=ler.nextInt();
+ prod=a*b;
+ System.out.println("PROD = "+ prod);
+        
+    }
+ 
+}
